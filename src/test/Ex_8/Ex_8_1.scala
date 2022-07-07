1695a9194da3941bb08f850a93c0764ea89a84c9
P-JSL <althsuwpfl@gmail.com>
2022-07-07 17:26:23 +0900

    example 5,6,7,8

--- src/test/Ex_8/Ex_8_1.scala
+++ src/test/Ex_8/Ex_8_1.scala
@@ -1,5 +1,11 @@
 package test.Ex_8
 
 object Ex_8_1 {
-  
+  def main(args: Array[String]): Unit = {
+    val o = List(1,2,3,4)
+    println(o)
+    
+    val n= o.map(i => i * 10)
+    println(n)
+  }
 }
